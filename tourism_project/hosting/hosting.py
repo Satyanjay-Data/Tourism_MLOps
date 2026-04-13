e4ff41226876bedddb9e5fc01da3ef8b16cf2a14
Satyanjay-Data <023512@gmail.com>
2026-04-13 12:28:01 +0530

    Update hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -3,10 +3,10 @@ from huggingface_hub import HfApi
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 
-folder_path = "tourism_project/deployment"  # ✅ FIXED PATH
+folder_path = "tourism_project/deployment" 
 
 api.upload_folder(
     folder_path=folder_path,
-    repo_id="Satyanjay/tourism-package-prediction-model",
-    repo_type="model",
+    repo_id="Satyanjay/Tourism-Package-Prediction",
+    repo_type="space",
 )
